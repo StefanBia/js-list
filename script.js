7b5001bf1f80221c998d387587c15db755cade22
Stefan Bia <stefanbia0108@gmail.com>
2024-03-14 10:51:35 +0200

    Add elements on button click, delete with individual delete button

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-const input = document.querySelector(input);
-const button = document.querySelector(button);
+const input = document.getElementById('item');
+const button = document.getElementById('add-button');
+const list = document.getElementById('sh-list');
+
+button.addEventListener('click', function(){
+    
+    let inputString = input.value;
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const newButton = document.createElement("button");
+    span.textContent = inputString;
+    newButton.textContent = "delete";
+    newButton.addEventListener('click', function(){
+        list.removeChild(li);
+    });
+    li.appendChild(span);
+    li.appendChild(newButton);
+    list.appendChild(li);
+    input.value = '';
+    console.log(inputString);
+});
